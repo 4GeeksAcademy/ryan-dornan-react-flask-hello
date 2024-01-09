139e722ad8e817bfde34813a2639ebed065df32a
ryandornan <116069884+ryandornan@users.noreply.github.com>
2024-01-09 23:27:10 +0000

    First submission of authentciation app using Flask & React

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,8 +3,12 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, get_hash
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, get_jwt_identity, jwt_required
+from flask_jwt_extended import create_access_token
+
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,38 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    found_user = User.query.filter_by(email = email, password = get_hash(password)).one_or_none() 
+    #found_user = User.query.filter_by(email = email).one_or_none() 
+    
+    if found_user is None:
+        return "email or password incorrect", 400
+
+    #if username != "test" or password != "test":
+     #   return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@api.route("/get-hash", methods=["POST"])
+def handle_get_hash():
+    to_hash = request.json.get("string")
+    return get_hash(to_hash)
+    
